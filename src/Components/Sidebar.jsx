26e806c0d0b9c86b5e69bd1a13286b70644050ed
22HOP2E-Enkhbilguun <22hop2e-Enkhbilguun@Themuulns.local>
2022-12-03 12:15:02 +0800

    add

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,15 +1,70 @@
-import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import {BsSearch} from "react-icons/bs";
+import {AiOutlineCompass} from "react-icons/ai";
+import {RiMessengerLine} from "react-icons/ri";
+import {AiOutlineHeart} from "react-icons/ai"
 
-function Sidebar() {
+const Navbar = () => {
   return (
-    <>
-    <div className="sidebar">
-      {/* <span className="logo">Instagram</span>
-      <input className="search" type="text" placeholder="Search" />
-      <div className="emojiholder"></div>       */}
+    <div className="navbar">
+      <div className="buttonLogo">
+        <img
+          src=""
+          style={{
+            width: "103px",
+            height: "39px",
+            marginLeft: "20px",
+            marginTop: "15px",
+          }}
+          alt=""
+        />
+      </div>
+      <div className="navbarButtonContainer">
+        <div className="navbarButton">
+          <div className="navbarImg">
+            <AiFillHome style={{ width: "32px", height: "32px" }} />
+          </div>
+          <span className="buttonName">Home</span>
+        </div>
+        <div className="navbarButton">
+          <div className="navbarImg">
+          <BsSearch style={{ width: "32px", height: "32px" }} />
+          </div>
+          <span className="buttonName">Search</span>
+        </div>
+        <div className="navbarButton">
+          <div className="navbarImg">
+          <AiOutlineCompass style={{ width: "32px", height: "32px" }} />
+          </div>{" "}
+          <span className="buttonName">Explore</span>
+        </div>
+        <div className="navbarButton">
+          <div className="navbarImg">
+          <AiOutlineHeart style={{ width: "32px", height: "32px" }} />
+          </div>{" "}
+          <span className="buttonName">Messages</span>
+        </div>
+        <div className="navbarButton">
+          <div className="navbarImg">
+            <img src="" style={{ width: "22px", height: "22px" }} alt="" />
+          </div>
+          <span className="buttonName">Notifications</span>
+        </div>
+        <div className="navbarButton">
+          <div className="navbarImg">
+            <img src="" style={{ width: "22px", height: "22px" }} alt="" />
+          </div>
+          <span className="buttonName">Create</span>
+        </div>
+        <div className="navbarButton">
+          <div className="navbarImg">
+            <img src="" style={{ width: "22px", height: "22px" }} alt="" />
+          </div>
+          <span className="buttonName">Profile</span>
+        </div>
+      </div>
+      <div className="more"></div>
     </div>
-    </>
   );
-}
-
-export default Sidebar;
+};
+export default Navbar;
